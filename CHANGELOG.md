fce73b6eda0833e041047d1403bc173ad4ac63aa
lzw1998 <642926134@qq.com>
2022-04-21 19:41:12 +0800

    feat: 🎸 init

---
+++ CHANGELOG.md
@@ -0,0 +1,43 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+## [0.3.0](https://github.com/lzw1998/leven-react-template/compare/v0.2.0...v0.3.0) (2022-04-21)
+
+
+### ⚠ BREAKING CHANGES
+
+* 🧨 版本更新
+
+### ♻️ Code Refactoring | 代码重构
+
+* 💡 移除非必要的项目文件 ([7d8d458](https://github.com/lzw1998/leven-react-template/commit/7d8d4584763d9da23d0f8a974ec62ba514610962))
+
+## [0.2.0](https://github.com/lzw1998/leven-react-template/compare/v0.1.0...v0.2.0) (2022-04-21)
+
+
+### ⚠ BREAKING CHANGES
+
+* 🧨 完成模板项目搭建
+
+### ✨ Features | 新功能
+
+* 🎸 添加react路由、axios、classnames等包依赖，添加webpack一些优化plugin ([565d8fe](https://github.com/lzw1998/leven-react-template/commit/565d8fe798d296dc335ccf34c31b3a8f3e6a5f8a))
+
+## [0.1.0](https://github.com/lzw1998/leven-react-template/compare/v0.0.2...v0.1.0) (2022-04-21)
+
+
+### ⚠ BREAKING CHANGES
+
+* 🧨 基本完成模板项目的搭建
+
+### 🐛 Bug Fixes | Bug 修复
+
+* 🐛 修改react17以后不引入React报错的相关问题 ([85afa93](https://github.com/lzw1998/leven-react-template/commit/85afa93dabac2ad4e73581b778323913757c099a))
+
+### 0.0.2 (2022-04-19)
+
+
+### ✨ Features | 新功能
+
+* 🎸 init bf39fe1
---
+++ config/webpack.common.js
@@ -0,0 +1,123 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const WebpackBar = require('webpackbar');
+const paths = require('./paths');
+const { NODE_ENV, moduleFileExtensions } = require('./constants');
+
+const isEnvDevelopment = NODE_ENV === 'development';
+// const isEnvProduction = NODE_ENV === 'production';
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+
+console.log(NODE_ENV);
+module.exports = {
+  entry: paths.appIndexJs,
+  resolve: {
+    extensions: moduleFileExtensions.map((ext) => `.${ext}`),
+    alias: {
+      src: paths.appSrc,
+      components: paths.appSrcComponents,
+      utils: paths.appSrcUtils,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?|jsx?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [['@babel/plugin-transform-runtime']],
+          },
+        },
+      },
+      {
+        test: cssRegex,
+        exclude: cssModuleRegex,
+        use: [
+          isEnvDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          { loader: 'postcss-loader', options: { postcssOptions: { plugins: [['autoprefixer']] } } },
+        ],
+      },
+      {
+        test: cssModuleRegex,
+        use: [
+          isEnvDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { modules: { mode: 'local', localIdentName: '[hash:base64:6]' } } },
+          { loader: 'postcss-loader', options: { postcssOptions: { plugins: [['autoprefixer']] } } },
+        ],
+      },
+      {
+        test: sassRegex,
+        exclude: sassModuleRegex,
+        use: [
+          isEnvDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          { loader: 'postcss-loader', options: { postcssOptions: { plugins: [['autoprefixer']] } } },
+          'sass-loader',
+        ],
+      },
+      {
+        test: sassModuleRegex,
+        use: [
+          isEnvDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { modules: { mode: 'local', localIdentName: '[hash:base64:6]' } } },
+          { loader: 'postcss-loader', options: { postcssOptions: { plugins: [['autoprefixer']] } } },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf?)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[contenthash][ext]',
+        },
+      },
+      {
+        test: /\.png$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[contenthash][ext]',
+        },
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: paths.appHtml, filename: 'index.html', inject: 'body' }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          context: paths.appPublic,
+          from: '*',
+          to: paths.appBuild,
+          toType: 'dir',
+          globOptions: {
+            dot: true,
+            gitignore: true,
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
+    }),
+    new WebpackBar({
+      name: isEnvDevelopment ? 'RUNNING' : 'BUNDLING',
+      color: isEnvDevelopment ? '#52c41a' : '#722ed1',
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        configFile: paths.appTsConfig,
+      },
+    }),
+  ],
+};
